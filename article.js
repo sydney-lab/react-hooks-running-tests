387c855f8eaf00670cfc811ed6b968dcf9f5c2fe
sydney <sydneyosindi03@gmail.com>
2025-07-14 09:58:03 +0300

    second commit

--- article.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Article() {
-  return <div>please pass this test</div>;
-}
-
-export default Article;
--- header.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Header() {
-  return <h1>hello from the Header!</h1>;
-}
-
-export default Header;
